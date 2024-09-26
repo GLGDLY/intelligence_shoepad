3f889092ae7461fab3ce35a8af2d1bbcd26f5823
GLGDLY <tzlgdly@gmail.com>
2024-09-26 20:57:28 +0800

    rm mainboard

--- mainboard/main/include/macro_utils.h
+++
@@ -1,6 +0,0 @@
-#ifndef _MACRO_UTILS_H
-#define _MACRO_UTILS_H
-
-#define X_EXPAND_CNT(...) +1
-
-#endif // _MACRO_UTILS_H
--- mainboard/main/include/mqtt_app.h
+++
@@ -1,6 +0,0 @@
-#ifndef _MQTT_APP_H
-#define _MQTT_APP_H
-
-void mqtt5_app_start(void);
-
-#endif // _MQTT_APP_H
